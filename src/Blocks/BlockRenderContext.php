b0574197c7dd0443c5eeea072d5e9430c4c9128c
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 06:22:08 +0700

    refactor: move render context into package namespace

---
+++ src/Blocks/BlockRenderContext.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zaengit\PageBuilder\Blocks;
+
+final readonly class BlockRenderContext
+{
+    public function __construct(
+        public string $blockId,
+        public string $blockType,
+        public array $attrs,
+        public mixed $data,
+        public bool $preview,
+    ) {}
+}
