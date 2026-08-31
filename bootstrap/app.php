4305043f97bc44ddf922e8e5f34422849e81ef76
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 05:56:07 +0700

    feat: register security headers middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\SecurityHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {})
+    ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->alias([
+            'security.headers' => SecurityHeaders::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {})
     ->create();
