a0c30cfeeb3fed0be7e55fc6f1abd2ec7e85a08b
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 00:29:34 +0700

    feat: protect builder API with session auth

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,25 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BlockController;
 use App\Http\Controllers\PageController;
+use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/blocks', [BlockController::class, 'index']);
-Route::post('/render-block', [BlockController::class, 'render']);
-Route::post('/render-page', [BlockController::class, 'renderPage']);
-Route::get('/pages/{page}', [PageController::class, 'show']);
-Route::post('/pages', [PageController::class, 'store']);
-Route::put('/pages/{page}', [PageController::class, 'update']);
-Route::post('/pages/{page}/publish', [PageController::class, 'publish']);
+Route::middleware(StartSession::class)->group(function (): void {
+    Route::post('/auth/register', [AuthController::class, 'register']);
+    Route::post('/auth/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth')->group(function (): void {
+        Route::get('/auth/me', [AuthController::class, 'me']);
+        Route::post('/auth/logout', [AuthController::class, 'logout']);
+
+        Route::get('/blocks', [BlockController::class, 'index']);
+        Route::post('/render-block', [BlockController::class, 'render']);
+        Route::post('/render-page', [BlockController::class, 'renderPage']);
+        Route::get('/pages/{page}', [PageController::class, 'show']);
+        Route::post('/pages', [PageController::class, 'store']);
+        Route::put('/pages/{page}', [PageController::class, 'update']);
+        Route::post('/pages/{page}/publish', [PageController::class, 'publish']);
+    });
+});
