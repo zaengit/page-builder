322ad9440af92b33a41a581125a1b576890f18d4
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 06:24:26 +0700

    refactor: leave app web routes to host application

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,3 @@
 <?php
 
-use App\Http\Controllers\BlockAssetController;
-use App\Http\Controllers\PreviewController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/page-builder/preview', [PreviewController::class, 'show'])->name('page-builder.preview');
-
-Route::get('/block-assets/{namespace}/{block}/{asset}', [BlockAssetController::class, 'show'])
-    ->where([
-        'namespace' => '[a-z0-9-]+',
-        'block' => '[a-z0-9-]+',
-        'asset' => '[A-Za-z0-9._-]+',
-    ]);
+// Development harness only. Page Builder routes are loaded by PageBuilderServiceProvider.
