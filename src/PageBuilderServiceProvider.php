adf00aeddef750af58fce55e0fb985ce7a5b13a1
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 06:21:22 +0700

    feat: add package service provider

---
+++ src/PageBuilderServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Zaengit\PageBuilder;
+
+use Illuminate\Support\ServiceProvider;
+use Zaengit\PageBuilder\Blocks\AssetCollector;
+use Zaengit\PageBuilder\DataProviders\DataProviderRegistry;
+
+final class PageBuilderServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/page-builder.php', 'page-builder');
+        $this->app->scoped(AssetCollector::class);
+        $this->app->singleton(DataProviderRegistry::class, fn () => new DataProviderRegistry());
+    }
+
+    public function boot(): void
+    {
+        $this->loadRoutesFrom(__DIR__.'/../routes/page-builder.php');
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'page-builder');
+
+        $this->publishes([
+            __DIR__.'/../config/page-builder.php' => config_path('page-builder.php'),
+        ], 'page-builder-config');
+    }
+}
