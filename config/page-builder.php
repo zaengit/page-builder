d5f652b9c558517fe2db8cdaa9cb51dbdf3dea79
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 06:28:27 +0700

    feat: add host middleware hooks

--- config/page-builder.php
+++ config/page-builder.php
@@ -8,4 +8,6 @@ return [
     'custom_blocks_path' => env('PAGE_BUILDER_BLOCKS_PATH', base_path('blocks')),
     'route_prefix' => env('PAGE_BUILDER_ROUTE_PREFIX', 'page-builder'),
     'api_prefix' => env('PAGE_BUILDER_API_PREFIX', 'api/page-builder'),
+    'middleware' => [],
+    'asset_middleware' => [],
 ];
