ee70445d2b97c2b74f910fceba4268237b5f264c
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 06:27:43 +0700

    feat: separate package core and host custom block paths

--- config/page-builder.php
+++ config/page-builder.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
-    'blocks_path' => env('PAGE_BUILDER_BLOCKS_PATH', base_path('blocks')),
+    'block_paths' => [
+        dirname(__DIR__).'/blocks',
+        env('PAGE_BUILDER_BLOCKS_PATH', base_path('blocks')),
+    ],
+    'custom_blocks_path' => env('PAGE_BUILDER_BLOCKS_PATH', base_path('blocks')),
     'route_prefix' => env('PAGE_BUILDER_ROUTE_PREFIX', 'page-builder'),
     'api_prefix' => env('PAGE_BUILDER_API_PREFIX', 'api/page-builder'),
 ];
