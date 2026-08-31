dd7fc39fd402599de15afa6c798216afe3ebeb7d
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 00:08:20 +0700

    feat: add data provider registry

---
+++ app/DataProviders/DataProviderRegistry.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataProviders;
+
+use InvalidArgumentException;
+
+final class DataProviderRegistry
+{
+    /** @var array<string, class-string<BlockDataProvider>> */
+    private array $providers = [];
+
+    public function register(string $name, string $provider): void
+    {
+        if (!is_subclass_of($provider, BlockDataProvider::class)) {
+            throw new InvalidArgumentException("Data provider [{$provider}] must implement BlockDataProvider.");
+        }
+
+        $this->providers[$name] = $provider;
+    }
+
+    public function has(string $name): bool
+    {
+        return isset($this->providers[$name]);
+    }
+
+    public function resolve(string $name): BlockDataProvider
+    {
+        if (!$this->has($name)) {
+            throw new InvalidArgumentException("Unknown data provider [{$name}].");
+        }
+
+        return app($this->providers[$name]);
+    }
+}
