fec314d4107f8d2cc416599d67ab2ae98b72ef5e
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 06:23:02 +0700

    refactor: move block controller into package namespace

---
+++ src/Http/Controllers/BlockController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Zaengit\PageBuilder\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Zaengit\PageBuilder\Blocks\BlockRegistry;
+use Zaengit\PageBuilder\Blocks\PageContentValidator;
+use Zaengit\PageBuilder\Blocks\PageRenderer;
+
+final class BlockController
+{
+    public function index(BlockRegistry $registry): JsonResponse
+    {
+        $blocks = array_values(array_map(function (array $definition): array {
+            foreach (array_keys($definition) as $key) if (str_starts_with((string) $key, '_')) unset($definition[$key]);
+            return $definition;
+        }, $registry->all()));
+        return response()->json($blocks);
+    }
+
+    public function render(Request $request, PageRenderer $renderer, PageContentValidator $validator): JsonResponse
+    {
+        $content = $validator->validate(['blocks'=>[$request->all()]]);
+        $block = $content['blocks'][0];
+        $html = $renderer->render($content, true);
+        return response()->json(['id'=>$block['id'], 'html'=>$html, 'assets'=>$renderer->assets()]);
+    }
+
+    public function renderPage(Request $request, PageRenderer $renderer, PageContentValidator $validator): JsonResponse
+    {
+        $content = $validator->validate($request->all());
+        $html = $renderer->render($content, true);
+        return response()->json(['html'=>$html, 'assets'=>$renderer->assets()]);
+    }
+}
