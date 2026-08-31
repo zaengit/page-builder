bce66f2a052acea0555cf27c5bb1e8483524b078
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 00:03:00 +0700

    feat: add nested container template

---
+++ blocks/container/template.blade.php
@@ -0,0 +1,7 @@
+<section
+    data-block="core/container"
+    data-block-id="{{ $blockId }}"
+    style="max-width: {{ $attrs['maxWidth'] }}; margin-inline: auto; padding: {{ $attrs['padding'] }};"
+>
+    {!! $children !!}
+</section>
