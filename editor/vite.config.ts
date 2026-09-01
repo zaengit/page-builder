05035959efb11ee70e3a0a616b30136359addcff
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-01 07:44:59 +0700

    feat: enable tailwind vite plugin

--- editor/vite.config.ts
+++ editor/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
 const root = import.meta.dirname;
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   build: {
     outDir: resolve(root, '../resources/dist'),
     emptyOutDir: true,
