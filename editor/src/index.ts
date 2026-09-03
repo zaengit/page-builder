2a0f4fece93b212999ead87c60b94f978f345f1b
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-03 23:26:48 +0700

    editor: expose public engine-neutral API

---
+++ editor/src/index.ts
@@ -0,0 +1,6 @@
+export { EditorApp } from './EditorApp';
+export { mountPageBuilder } from './public-api';
+export { createHttpHostAdapter, createStandaloneHostAdapter } from './host-adapter';
+export type { EditorHostAdapter, RenderOutput, RenderDiagnostic, RenderAssetSet, DatasourceMetadata, MediaItem, StandaloneHostOptions } from './host-adapter';
+export type * from './types';
+export { initializeExtensionApi } from './registry';
