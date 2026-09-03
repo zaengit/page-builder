566585a3d85659bb2deb26e529b79e1977b562d1
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-03 22:14:36 +0700

    refactor: move rendering contract into Laravel engine

--- src/Rendering/RenderingEngine.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Zaengit\PageBuilder\Rendering;
-
-interface RenderingEngine
-{
-    /** @param array<string, mixed> $page */
-    public function render(array $page): RenderResult;
-}
