1398e5b2e8e73fd14c52f3ec2033666baf060d70
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-03 23:35:18 +0700

    go: declare supported universal contract versions

--- engine/go/version.go
+++ engine/go/version.go
@@ -2,5 +2,9 @@ package pagebuilder
 
 const ProtocolVersion = 1
 const SpecificationVersion = 1
+const PageVersion = 1
+const BlockManifestVersion = 1
+const DatasourceVersion = 1
+const TemplateLanguageVersion = 1
 
 var EngineVersion = "dev"
