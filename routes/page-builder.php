0621968efc5a22fb10e8ad9d24b6edb39e70796c
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-03 09:33:48 +0700

    Add media library routes

--- routes/page-builder.php
+++ routes/page-builder.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Zaengit\PageBuilder\Http\Controllers\BlockAssetController;
 use Zaengit\PageBuilder\Http\Controllers\BlockController;
 use Zaengit\PageBuilder\Http\Controllers\EditorAssetController;
+use Zaengit\PageBuilder\Http\Controllers\MediaController;
 use Zaengit\PageBuilder\Http\Controllers\PreviewController;
 
 $middleware = config('page-builder.middleware', []);
@@ -15,6 +16,11 @@ Route::middleware(is_array($middleware) ? $middleware : [$middleware])->group(fu
         Route::get('/blocks', [BlockController::class, 'index'])->name('page-builder.blocks');
         Route::post('/render-block', [BlockController::class, 'render'])->name('page-builder.render-block');
         Route::post('/render-page', [BlockController::class, 'renderPage'])->name('page-builder.render-page');
+        Route::get('/media', [MediaController::class, 'index'])->name('page-builder.media.index');
+        Route::post('/media', [MediaController::class, 'store'])->name('page-builder.media.store');
+        Route::delete('/media/{media}', [MediaController::class, 'destroy'])
+            ->where('media', '[A-Za-z0-9._-]+')
+            ->name('page-builder.media.destroy');
     });
 
     Route::get('/'.$routePrefix.'/preview', [PreviewController::class, 'show'])
@@ -25,6 +31,10 @@ Route::middleware(is_array($assetMiddleware) ? $assetMiddleware : [$assetMiddlew
     Route::get('/'.$routePrefix.'/assets/{asset}', [EditorAssetController::class, 'show'])
         ->where('asset', '[A-Za-z0-9._-]+')
         ->name('page-builder.editor-asset');
+
+    Route::get('/'.$routePrefix.'/media/{media}', [MediaController::class, 'show'])
+        ->where('media', '[A-Za-z0-9._-]+')
+        ->name('page-builder.media.show');
 
     Route::get('/block-assets/{namespace}/{block}/{asset}', [BlockAssetController::class, 'show'])
         ->where([
