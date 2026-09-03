e07a1f1b5a09984efd4488852fac17a06a610522
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-03 23:30:51 +0700

    laravel: declare supported universal contract versions

---
+++ engine/laravel/src/EngineVersion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Zaengit\PageBuilder\Engine\Laravel;
+
+final class EngineVersion
+{
+    public const ENGINE = '0.1.0';
+    public const SPECIFICATION = 1;
+    public const PAGE = 1;
+    public const BLOCK = 1;
+    public const DATASOURCE = 1;
+    public const RENDERER_PROTOCOL = 1;
+    public const TEMPLATE_LANGUAGE = 1;
+}
