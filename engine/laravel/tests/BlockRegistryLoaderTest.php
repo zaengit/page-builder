f3adfb4a00a161041356011b1e250828bf68a6bb
Zaen <31380073+zaengit@users.noreply.github.com>
2026-09-03 22:08:26 +0700

    test(laravel-engine): move block registry tests under engine

---
+++ engine/laravel/tests/BlockRegistryLoaderTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Engine\Laravel;
+
+use Tests\TestCase;
+use Zaengit\PageBuilder\Engine\Laravel\Runtime\BlockRegistryLoader;
+
+final class BlockRegistryLoaderTest extends TestCase
+{
+    public function test_all_builtin_blocks_have_portable_templates(): void
+    {
+        $registry = app(BlockRegistryLoader::class)->load([base_path('blocks')]);
+
+        foreach (['core/heading', 'core/image', 'core/container', 'core/columns', 'core/carousel'] as $name) {
+            $this->assertArrayHasKey($name, $registry);
+            $this->assertIsString($registry[$name]['_template']);
+            $this->assertFileExists($registry[$name]['_template']);
+        }
+    }
+}
